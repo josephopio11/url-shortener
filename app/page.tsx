d1ddcc4b3525d9f317618d463ad5c0be42f5b5e5
Joseph Opio <josephopio11@gmail.com>
2023-11-27 09:31:08 +0000

    added all dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,227 @@
+import CreateShortUrls from '@/app/createPublic'
+import { db } from '@/lib/db'
+import { shortUrls, users } from '@/lib/db/schema'
+import { Anchor, ArrowDown, ExternalLink, Star, User } from 'lucide-react'
 import Image from 'next/image'
+import Link from 'next/link'
 
-export default function Home() {
+export const revalidate = 60
+
+export default async function Page() {
+  const getUsers = await db.select().from(users)
+  const getShortUrls = await db.select().from(shortUrls)
+  const githubInfo = await fetch(
+    'https://api.github.com/repos/nrjdalal/rdt-li',
+  ).then((res) => res.json())
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+    <main className="container flex max-w-screen-md flex-col items-center px-5">
+      <section className="space-y-6 pb-48 pt-12 lg:py-32">
+        <div className="flex w-full max-w-[64rem] flex-col items-center gap-4 text-center">
+          <Link
+            href="https://rdt.li/x-nrjdalal"
+            className="rounded-2xl border bg-background px-4 py-1.5 text-xs font-medium"
             target="_blank"
-            rel="noopener noreferrer"
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            @nrjdalal&apos;s twitter
+          </Link>
+
+          <h1 className="font-heading max-w-md text-3xl sm:text-4xl">
+            <span className="font-mono font-semibold">
+              {process.env.NEXT_PUBLIC_APP_URL?.split('://')[1]}
+            </span>{' '}
+            self hostable open source URL shortener
+          </h1>
+
+          <CreateShortUrls />
+
+          <p className="max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
+            {process.env.NEXT_PUBLIC_APP_URL?.split('://')[1]} self hostable,
+            feature rich, minimalistic and open source URL shortener. Built with
+            Next.js, Drizzle, NextAuth and Postgres.
+          </p>
+
+          <p className="text-xs">
+            Try now or <span className="font-semibold">self host</span> (check
+            GitHub for more information)
+          </p>
+
+          <div className="mt-3 flex space-x-4">
+            <Link
+              href="/x"
+              className="flex h-10 items-center rounded-md bg-blue-500 px-8 text-sm text-white"
+            >
+              Login
+            </Link>
+            <Link
+              href="https://rdt.li/github"
+              target="_blank"
+              rel="noreferrer"
+              className="relative flex h-10 items-center gap-2 rounded-md border bg-background px-8 text-sm"
+            >
+              <p>Github</p>
+              <div className="absolute -top-4 right-2.5 flex items-center gap-1 rounded-md bg-foreground px-2 text-background">
+                <Star className="h-2.5 w-2.5" />
+                <p className="pt-0.5 font-mono text-[0.6rem] font-bold">
+                  {githubInfo?.stargazers_count || 0}
+                </p>
+              </div>
+            </Link>
+          </div>
         </div>
+      </section>
+
+      {/* 
+        // ~ Some stats
+      */}
+      <div className="mb-20 grid grid-cols-2 gap-4 text-black">
+        <h3 className="col-span-2 flex items-center justify-center text-xl font-medium text-blue-500">
+          <ArrowDown className="text-blue-200" />
+          <span className="ml-1 text-blue-500">Some Stats</span>
+          <ArrowDown className="text-blue-300" />
+        </h3>
+
+        <div className="flex flex-col items-center space-y-1 rounded-md border-2 border-green-400 bg-green-100 px-8 py-3">
+          <User className="h-8 w-8 text-green-600" />
+          <p className="text-xs">Users</p>
+          <p>{getUsers?.length}</p>
+        </div>
+        <div className="flex flex-col items-center space-y-1 rounded-md border-2 border-blue-400 bg-blue-100 px-8 py-3">
+          <Anchor className="h-8 w-8 text-blue-600" />
+          <p className="text-xs">Short Links</p>
+          <p>{getShortUrls?.length}</p>
+        </div>
+        {(process.env.NEXT_PUBLIC_UMAMI_URL ||
+          process.env.NODE_ENV === 'development') && (
+          <Link
+            className="col-span-2 flex flex-col items-center space-y-1 rounded-md border-2 border-red-400 bg-red-100 px-8 py-2"
+            href={
+              `${process.env.NEXT_PUBLIC_UMAMI_SHARE_URL}` || 'https://umami.is'
+            }
+            target="_blank"
+          >
+            <p className="flex gap-1 text-xs">
+              Optional Analytics by{' '}
+              <svg
+                className="h-5 w-5 rounded-full"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 428 389.11"
+              >
+                <g data-name="Layer 2">
+                  <g data-name="Layer 4">
+                    <circle
+                      cx="214.15"
+                      cy="181"
+                      r="171"
+                      fill="#fff"
+                      stroke="#000"
+                      strokeMiterlimit="10"
+                      strokeWidth="20"
+                    ></circle>
+                    <path d="M413 134.11H15.29a15 15 0 0 0-15 15v15.3C.12 168 0 171.52 0 175.11c0 118.19 95.81 214 214 214 116.4 0 211.1-92.94 213.93-208.67 0-.44.07-.88.07-1.33v-30a15 15 0 0 0-15-15z"></path>
+                  </g>
+                </g>
+              </svg>{' '}
+              Umami
+              <ExternalLink className="h-4 w-4 text-red-400" />
+            </p>
+          </Link>
+        )}
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <h2 className="mb-2 flex cursor-default items-center text-xl font-medium">
+        <ArrowDown className="text-blue-200" />
+        <span className="ml-1 text-blue-500">Image Demo</span>
+        <ArrowDown className="text-blue-300" />
+      </h2>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <Image
+        src={'/ABA1DFD8-2816-4509-8691-DF95F7D82EB6.jpeg'}
+        alt="rdt.li screenshot"
+        width={1080}
+        height={1080}
+      />
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
+      <section id="open-source" className="container py-8 md:py-12 lg:py-24">
+        <div className="mx-auto flex max-w-[58rem] flex-col items-center justify-center gap-4 text-center">
+          <h2 className="font-heading text-3xl leading-[1.1] sm:text-3xl">
+            Proudly Open Source
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+          <p className="max-w-[85%] text-xs leading-normal text-muted-foreground">
+            Redirect.link is open source and powered by open source software.
+            The source code is available on{' '}
+            <Link
+              href="https://rdt.li/github"
+              target="_blank"
+              rel="noreferrer"
+              className="underline underline-offset-4"
+            >
+              GitHub
+            </Link>
+            .
           </p>
-        </a>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+          <div className="mt-4 flex gap-2">
+            <Link href="https://vercel.com" target="_blank">
+              <Image
+                src="/powered-by-vercel.svg"
+                alt="Powered by Vercel"
+                height={32}
+                width={128}
+              />
+            </Link>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+            {(process.env.NEXT_PUBLIC_UMAMI_URL ||
+              process.env.NODE_ENV === 'development') && (
+              <Link
+                className="flex h-[26.56px] items-center gap-2 rounded-sm bg-black px-2 text-[0.6rem] text-background"
+                href={
+                  `${process.env.NEXT_PUBLIC_UMAMI_SHARE_URL}` ||
+                  'https://umami.is'
+                }
+                target="_blank"
+              >
+                <svg
+                  className="rounded-full border border-white"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  viewBox="0 0 428 389.11"
+                >
+                  <g data-name="Layer 2">
+                    <g data-name="Layer 4">
+                      <circle
+                        cx="214.15"
+                        cy="181"
+                        r="171"
+                        fill="#fff"
+                        stroke="#000"
+                        strokeMiterlimit="10"
+                        strokeWidth="20"
+                      ></circle>
+                      <path d="M413 134.11H15.29a15 15 0 0 0-15 15v15.3C.12 168 0 171.52 0 175.11c0 118.19 95.81 214 214 214 116.4 0 211.1-92.94 213.93-208.67 0-.44.07-.88.07-1.33v-30a15 15 0 0 0-15-15z"></path>
+                    </g>
+                  </g>
+                </svg>
+                <p className="text-white">
+                  Analytics by <span className="font-medium">Umami</span>
+                </p>
+              </Link>
+            )}
+          </div>
+
+          <p className="text-xs">
+            Report abuse at this{' '}
+            <Link
+              className="border-b border-foreground/50"
+              href="mailto:rdt-li-abuse@nrjdalal.com"
+            >
+              email
+            </Link>
+            .
           </p>
-        </a>
-      </div>
+        </div>
+      </section>
     </main>
   )
 }
